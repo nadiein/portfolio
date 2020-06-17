a0f3c1d72ea17d73806043cda6e46a578cbc06d8
Igor <snoozzzy7@gmail.com>
2020-06-17 22:43:26 +0300

    Updated parallax and home view

--- client/web-app/src/app/components/home/home.component.ts
+++ client/web-app/src/app/components/home/home.component.ts
@@ -9,22 +9,8 @@ import { Parallax, Foreground } from './parallax/parallax.component';
 })
 export class HomeComponent implements OnInit {
 
-    parallax:Parallax;
-
     constructor(private dispatcher:GlobalEventDispatcherService) { }
 
-    ngOnInit() { 
-        this.dispatcher.dispatch().subscribe(el => console.log(el))
-
-        let screenOne = new Foreground('./../../assets/images/foreground1.png', 3);
-        let screenTwo = new Foreground('./../../assets/images/foreground2.png', 3);
-        let screenTree = new Foreground('./../../assets/images/foreground3.png', 3);
-        this.parallax = new Parallax();
-        this.parallax.width = window.screen.width;
-        this.parallax.perspective = 8;
-        this.parallax.background = './../../assets/images/background1.png';
-        this.parallax.foreground = [];
-        console.log(this.parallax)
-    }
+    ngOnInit() { }
 
 }
