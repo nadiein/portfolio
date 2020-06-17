c564241ec02b2facdc6798f4e49f2dee0c912aaa
Igor <snoozzzy7@gmail.com>
2020-06-17 22:44:12 +0300

    Updated main view

--- client/web-app/src/app/app.component.ts
+++ client/web-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Parallax, Foreground } from './components/home/parallax/parallax.component';
+import { GlobalEventDispatcherService } from './services/global.event.dispatcher.service';
 
 @Component({
     selector: 'app-root',
@@ -6,4 +8,25 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+    parallax:Parallax;
+
+    constructor(private dispatcher:GlobalEventDispatcherService) { }
+
+    ngOnInit() { 
+        this.dispatcher.dispatch().subscribe(el => console.log(el))
+
+        let screenOne = new Foreground('./../../assets/images/foreground1.png', 3);
+        let screenTwo = new Foreground('./../../assets/images/foreground2.png', 3);
+        let screenTree = new Foreground('./../../assets/images/foreground3.png', 3);
+        this.parallax = new Parallax();
+        this.parallax.width = window.screen.width;
+        this.parallax.perspective = 8;
+        this.parallax.background = './../../assets/images/background1.png';
+        this.parallax.foreground = [
+            // screenOne,
+            // screenTwo,
+            // screenTree
+        ];
+        console.log(this.parallax)
+    }
 }
